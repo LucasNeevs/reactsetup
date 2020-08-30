6301965fef08cf620842fa034b7181767305a934
LucasNeevs <lucasneves.dev@gmail.com>
2020-08-30 10:34:27 -0300

    A few changes maded

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const Home: React.FC = () => (
 
     <main>
       <h1>
-        Welcome to <a href="https://nextjs.org">Next.js</a>
+        Welcome to <a href="https://nextjs.org">Next.js :)</a>
       </h1>
     </main>
   </div>
